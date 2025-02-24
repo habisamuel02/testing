2f96c1914be92af06556d50e082dd9cc8697d8f3
habisamuel02 <habisamuel02@gmail.com>
2025-02-24 19:07:32 +0300

    my first message

---
+++ script.js
@@ -0,0 +1,15 @@
+let button = document.querySelector('#btn')
+
+
+// function greet(){
+//     console.log("hello user")
+// }
+
+
+// button = addEventListener('click', greet);
+
+
+
+let form = document.querySelector('#frm')
+form = colorChange('click')
+form = Style.background = Red
